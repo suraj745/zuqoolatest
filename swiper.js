c89b357ac05795cc4df694c5409cae1d81df7b19
suraj <surajarya745@gmail.com>
2022-08-16 12:03:54 +0530

    logo-swiper

---
+++ swiper.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".swiper_carousel_logo", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: true,
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    "@0.75": {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    "@1.00": {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    "@1.50": {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+});
